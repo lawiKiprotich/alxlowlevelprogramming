ac8fffe8b37c310c8a1d67295fd5afa09f9d6e48
lawiKiprotich <fleeklawi@gmail.com>
2022-06-05 08:41:23 -0700

    GCC compiled

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,21 +1,4 @@
-#include <stdio.h>
-
-
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
-{
-
-    return (0);
-
+#include<stdio.h>
+int main(Void) {
+	return(0);
 }
